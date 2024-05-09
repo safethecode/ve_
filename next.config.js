cafe2a1a2ecad2e9f49c767194f56c4517554eef
safethecode <iam@sonjim.in>
2024-05-09 23:22:08 +0900

    feat(next): reactStrictMode true

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   experimental: {
     useDeploymentId: true,
     useDeploymentIdServerActions: true,
